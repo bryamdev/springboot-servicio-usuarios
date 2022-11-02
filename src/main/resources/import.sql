550100de78425e38500d334c95b0c69bb93f505c
Bryam Castaneda <bryamdev.contact@gmail.com>
2022-11-01 21:40:50 -0500

    adding encrypted user passwords

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO usuarios(username, password, enabled, nombre, apellido, email) VALUES ('admin', '1234', 1, 'admin', 'admin', 'admin@mail.com');
-INSERT INTO usuarios(username, password, enabled, nombre, apellido, email) VALUES ('bryam', '12345', 1, 'Bryam', 'Castañeda', 'bryamrojas20@gmail.com');
+INSERT INTO usuarios(username, password, enabled, nombre, apellido, email) VALUES ('admin', '$2a$10$XJv2baJQhNYQQOduMyJnN.7nzmJO72/CXlwSHbCAOockj8YN8eNvG', 1, 'admin', 'admin', 'admin@mail.com');
+INSERT INTO usuarios(username, password, enabled, nombre, apellido, email) VALUES ('bryam', '$2a$10$e2MNNNUh6NOtJ8TFfVNZReJwZNxujTBiYITyBwqd8DuJDC4mlnHHW', 1, 'Bryam', 'Castañeda', 'bryamrojas20@gmail.com');
 
 INSERT INTO roles(nombre) VALUES ('ROLE_USER');
 INSERT INTO roles(nombre) VALUES ('ROLE_ADMIN');
